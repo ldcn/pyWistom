06b6d527bbc5cf8a6ac498179e61fa775e6c2915
Emir Alwan <emir.alwan@proximion.com>
2025-03-25 21:04:34 +0000

    remove test code

--- socket_client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-server_address = ('10.44.42.10', 12345)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(server_address)
-data = client_socket.recv(1024)
-print(f"Received from server: {data.decode()}")
-client_socket.close()
