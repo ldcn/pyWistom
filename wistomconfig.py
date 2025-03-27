6d62450fd70f2583336359cd928085c3c3dd266a
Emir Alwan <emir.alwan@proximion.com>
2025-03-27 00:19:52 +0000

    change api version in config

--- wistomconfig.py
+++ wistomconfig.py
@@ -2,5 +2,5 @@ HOST = "10.44.40.218"
 PORT = 7734
 USER_ID = "admin"
 PASSWORD = "admin"
-API_VERSION = "LGIN" # API Version 1
-# API_VERSION = "API2" # API Version 2
+# API_VERSION = "LGIN" # API Version 1
+API_VERSION = "API2" # API Version 2
