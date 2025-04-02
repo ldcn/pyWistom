9b1a1369b18c9d0dc6c3c785b9c612071618394d
Emir Alwan <emir.alwan@proximion.com>
2025-04-02 20:05:48 +0000

    add test code for plotting data from API

---
+++ pyWistom_plottest.py
@@ -0,0 +1,19 @@
+from pyWistom import *
+import numpy as np
+
+with WistomClient(HOST, PORT, USER_ID, PASSWORD) as client:
+        login_response = client.login()
+        print("Login Response:", login_response)
+        spectrum_data = client.custom_api_request(COMMAND_ID['GET'], b'WSNS', b'DATA', bytes.fromhex("0a01"))
+        import matplotlib.pyplot as plt
+
+        # Assuming spectrum_data is a list or array of numerical values
+        if isinstance(spectrum_data['response']['spectrum_data_values'], (list, np.ndarray)):
+            plt.plot(spectrum_data['response']['spectrum_data_values'])
+            plt.title("Spectrum Data")
+            plt.xlabel("Index")
+            plt.ylabel("Value")
+            plt.grid(True)
+            plt.show()
+        else:
+            print("Spectrum data is not in a plottable format:", spectrum_data)
