468cfab5f0dd44dbb6cbe9e249f6bdb800933bb3
Emir Alwan <emir.alwan@proximion.com>
2025-04-04 22:28:32 +0000

    move connection and authentication methods to WistomConnection class, solves WS-5

--- wistomconnection.py
+++ wistomconnection.py
@@ -5,6 +5,7 @@ class WistomConnection:
         self.host = host
         self.port = port
         self.socket = None
+        self.token = 0
 
     def connect(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,3 +15,26 @@ class WistomConnection:
         if self.socket:
             self.socket.close()
             self.socket = None
+
+    def login(self, user_id, password, command_id, app_id, op_id):
+        user_id_bytes = user_id.encode('ascii')
+        password_bytes = password.encode('ascii')
+        data = user_id_bytes + b'\x00' + password_bytes + b'\x00'
+        payload = (
+            command_id
+            + self.token.to_bytes(2, 'big')
+            + app_id
+            + op_id
+            + len(data).to_bytes(4, 'big')
+            + data
+        )
+        self.socket.sendall(payload)
+        response = self.socket.recv(1024)
+        return response
+    
+    def increment_token(self):
+        self.token += 1
+        return self.token
+
+    def get_token(self):
+        return self.token
