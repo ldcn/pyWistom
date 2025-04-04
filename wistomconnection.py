4462678117cebae491b96548767e2328c49e7c3d
Emir Alwan <emir.alwan@proximion.com>
2025-04-04 16:07:15 +0000

    move connection related methods to new class WistomConnection

---
+++ wistomconnection.py
@@ -0,0 +1,16 @@
+import socket
+
+class WistomConnection:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = None
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+
+    def disconnect(self):
+        if self.socket:
+            self.socket.close()
+            self.socket = None
